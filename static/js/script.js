938f6e482c5f9e17c1a1ffd0864f4fa8e67320c2
THE1st <jdos46juan@gmail.com>
2024-08-30 20:51:30 -0500

    Se hace un experimento en los scripts.js y setting.js para hacer una configuracion con el tema claro y oscuro

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,58 @@
+// // window.onload = function () {
+// // 	let btn_close_userInfo, btn_change_theme;
+// // 	const body = document.querySelector("body");
+// // 	const userInfo = document.querySelector(".user-info");
+
+// // 	userInfo.addEventListener("click", function () {
+// // 		userInfo.classList.add("info-show");
+
+// // 		btn_close_userInfo = document.querySelector(".close-user-info");
+// // 		if (btn_close_userInfo) {
+// // 			btn_close_userInfo.addEventListener("click", function (e) {
+// // 				e.stopPropagation();
+// // 				userInfo.classList.remove("info-show");
+// // 			});
+// // 		}
+
+// // 		btn_change_theme = document.querySelector(".cambiar-tema");
+// // 		if (btn_change_theme) {
+// // 			btn_change_theme.addEventListener("click", function (e) {
+// // 				e.stopPropagation();
+// // 				toggleTheme();
+// // 			});
+// // 		}
+// // 	});
+
+// // 	function toggleTheme() {
+// // 		const htmlElement = document.documentElement;
+// // 		const setStyles = htmlElement.style;
+// // 		const getStyles = getComputedStyle(htmlElement);
+// // 		const oscuro = getStyles.getPropertyValue("--oscuro").toString();
+// // 		const gris = getStyles.getPropertyValue("--gris").toString();
+// // 		let theme = htmlElement.getAttribute("data-theme");
+// // 		theme = theme === "dark" ? "light" : "dark";
+// // 		htmlElement.setAttribute("data-theme", theme);
+// // 		if (theme === "dark") {
+// // 			setStyles.setProperty("--background-theme", oscuro);
+// // 		} else {
+// // 			setStyles.setProperty("--background-theme", gris);
+// // 		}
+// // 		applyThemeBody(theme);
+// // 	}
+
+// // 	function applyThemeBody(theme) {
+// // 		if (theme === "dark" && body) {
+// // 			body.classList.add("dark");
+// // 			body.classList.remove("light");
+// // 		} else {
+// // 			body.classList.add("light");
+// // 			body.classList.remove("dark");
+// // 		}
+// // 	}
+// // };
+
 // window.onload = function () {
-// 	let btn_close_userInfo, btn_change_theme;
+// 	let btn_close_userInfo;
 // 	const body = document.querySelector("body");
 // 	const userInfo = document.querySelector(".user-info");
 
@@ -13,13 +66,14 @@
 // 				userInfo.classList.remove("info-show");
 // 			});
 // 		}
+// 	});
 
-// 		btn_change_theme = document.querySelector(".cambiar-tema");
+// 	// Delegación de eventos para manejar el cambio de tema
+// 	body.addEventListener("click", function (e) {
+// 		const btn_change_theme = e.target.closest(".cambiar-tema");
 // 		if (btn_change_theme) {
-// 			btn_change_theme.addEventListener("click", function (e) {
-// 				e.stopPropagation();
-// 				toggleTheme();
-// 			});
+// 			e.stopPropagation();
+// 			toggleTheme();
 // 		}
 // 	});
 
@@ -51,10 +105,21 @@
 // 	}
 // };
 
+// Crear cuenta.
+
+// Iniciar sesión.
+
+// Retos.
+
+// Destinos turísticos.
+
 window.onload = function () {
 	let btn_close_userInfo;
 	const body = document.querySelector("body");
 	const userInfo = document.querySelector(".user-info");
+
+	// Al cargar la página, aplicar el tema almacenado en localStorage o el preferido por el navegador
+	applyStoredOrPreferredTheme();
 
 	userInfo.addEventListener("click", function () {
 		userInfo.classList.add("info-show");
@@ -86,6 +151,7 @@ window.onload = function () {
 		let theme = htmlElement.getAttribute("data-theme");
 		theme = theme === "dark" ? "light" : "dark";
 		htmlElement.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
 		if (theme === "dark") {
 			setStyles.setProperty("--background-theme", oscuro);
 		} else {
@@ -101,6 +167,24 @@ window.onload = function () {
 		} else {
 			body.classList.add("light");
 			body.classList.remove("dark");
+		}
+	}
+
+	function applyStoredOrPreferredTheme() {
+		const storedTheme = localStorage.getItem("theme");
+		const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+		const themeToApply = storedTheme || (prefersDarkScheme ? "dark" : "light");
+
+		document.documentElement.setAttribute("data-theme", themeToApply);
+		applyThemeBody(themeToApply);
+		const setStyles = document.documentElement.style;
+		const getStyles = getComputedStyle(document.documentElement);
+		const oscuro = getStyles.getPropertyValue("--oscuro").toString();
+		const gris = getStyles.getPropertyValue("--gris").toString();
+		if (themeToApply === "dark") {
+			setStyles.setProperty("--background-theme", oscuro);
+		} else {
+			setStyles.setProperty("--background-theme", gris);
 		}
 	}
 };
--- static/js/settings.js
+++ static/js/settings.js
@@ -1,27 +1,72 @@
+// const preferencesColorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+// const darkSchemeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+// const superElement = document.documentElement;
+// superElement.style.transition = "all 0.5s ease-in-out";
+// let themeSelected =
+// 	getAttributeDataTheme(superElement) === undefined
+// 		? preferencesColorScheme
+// 		: getAttributeDataTheme(superElement);
+
+// function applyTheme(theme) {
+// 	superElement.setAttribute("data-theme", theme);
+// 	toggleTheme();
+// }
+
+// function getAttributeDataTheme(element) {
+// 	let value = element.getAttribute("data-theme");
+// 	return value;
+// }
+
+// if (themeSelected === "dark") {
+// 	applyTheme("dark");
+// } else {
+// 	applyTheme("light");
+// }
+
+// darkSchemeMediaQuery.addEventListener("change", (event) => {
+// 	if (!event.matches) {
+// 		applyTheme("dark");
+// 		superElement.style.setProperty("--background_theme", "var(--oscuro)");
+// 	} else {
+// 		applyTheme("light");
+// 		superElement.style.setProperty("--background_theme", "var(--gris)");
+// 	}
+// });
+
+// function toggleTheme() {
+// 	const setStyles = superElement.style;
+// 	const getStyles = getComputedStyle(superElement);
+// 	const oscuro = getStyles.getPropertyValue("--oscuro").toString();
+// 	const gris = getStyles.getPropertyValue("--gris").toString();
+// 	let theme = superElement.getAttribute("data-theme");
+// 	theme = theme === "dark" ? "light" : "dark";
+// 	superElement.setAttribute("data-theme", theme);
+// 	if (theme === "dark") {
+// 		setStyles.setProperty("--background-theme", oscuro);
+// 	} else {
+// 		setStyles.setProperty("--background-theme", gris);
+// 	}
+// 	applyThemeBody(theme);
+// }
+
+// function applyThemeBody(theme) {
+// 	const body = document.querySelector(".body");
+// 	if (theme === "dark" && body) {
+// 		body.classList.add("dark");
+// 		body.classList.remove("light");
+// 	} else {
+// 		body.classList.add("light");
+// 		body.classList.remove("dark");
+// 	}
+// }
+
 const preferencesColorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const darkSchemeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 const superElement = document.documentElement;
 superElement.style.transition = "all 0.5s ease-in-out";
-let themeSelected =
-	getAttributeDataTheme(superElement) === undefined
-		? preferencesColorScheme
-		: getAttributeDataTheme(superElement);
 
-function applyTheme(theme) {
-	superElement.setAttribute("data-theme", theme);
-	toggleTheme();
-}
-
-function getAttributeDataTheme(element) {
-	let value = element.getAttribute("data-theme");
-	return value;
-}
-
-if (themeSelected === "dark") {
-	applyTheme("dark");
-} else {
-	applyTheme("light");
-}
+// Al cargar la página, aplicar el tema almacenado en localStorage o el preferido por el navegador
+applyStoredOrPreferredTheme();
 
 darkSchemeMediaQuery.addEventListener("change", (event) => {
 	if (!event.matches) {
@@ -33,6 +78,20 @@ darkSchemeMediaQuery.addEventListener("change", (event) => {
 	}
 });
 
+function applyStoredOrPreferredTheme() {
+	const storedTheme = localStorage.getItem("theme");
+	const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const themeToApply = storedTheme || (prefersDarkScheme ? "dark" : "light");
+
+	applyTheme(themeToApply);
+}
+
+function applyTheme(theme) {
+	superElement.setAttribute("data-theme", theme);
+	localStorage.setItem("theme", theme);
+	toggleTheme();
+}
+
 function toggleTheme() {
 	const setStyles = superElement.style;
 	const getStyles = getComputedStyle(superElement);
@@ -41,6 +100,7 @@ function toggleTheme() {
 	let theme = superElement.getAttribute("data-theme");
 	theme = theme === "dark" ? "light" : "dark";
 	superElement.setAttribute("data-theme", theme);
+	localStorage.setItem("theme", theme);
 	if (theme === "dark") {
 		setStyles.setProperty("--background-theme", oscuro);
 	} else {
@@ -50,7 +110,7 @@ function toggleTheme() {
 }
 
 function applyThemeBody(theme) {
-	const body = document.querySelector(".body");
+	const body = document.querySelector("body");
 	if (theme === "dark" && body) {
 		body.classList.add("dark");
 		body.classList.remove("light");
