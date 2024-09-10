e4c579f47719b7c7fb4a0079e38b9d04267cf981
THE1st <jdos46juan@gmail.com>
2024-09-09 19:55:37 -0500

    Se soluciona el error de que no dejaba subir las fotos de perfil fusionando el javascrip de perfilGaleria.js con scrips.js

--- static/js/script.js
+++ static/js/script.js
@@ -75,4 +75,31 @@ window.onload = function () {
 			setStyles.setProperty("--background-theme", gris);
 		}
 	}
+
+	// funcionalidades para el formulario de galeria para poder subir imagenes
+	const containerPerfilPicture = document.querySelector(".seleccion_foto_perfil");
+	const btnUploadImage = document.getElementById("id_foto_perfil");
+
+	if (containerPerfilPicture && btnUploadImage) {
+		containerPerfilPicture.addEventListener("click", function () {
+			btnUploadImage.click();
+		});
+
+		btnUploadImage.addEventListener("change", function () {
+			this.form.submit();
+		});
+	}
+
+	const containerPortadaPicture = document.querySelector(".seleccion_foto_portada");
+	const btnUploadImagePortada = document.getElementById("id_foto_portada");
+
+	if (containerPortadaPicture && btnUploadImagePortada) {
+		containerPortadaPicture.addEventListener("click", function () {
+			btnUploadImagePortada.click();
+		});
+
+		btnUploadImagePortada.addEventListener("change", function () {
+			this.form.submit();
+		});
+	}
 };
