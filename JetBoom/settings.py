107a50554a3369d3a9c66dcf5d53e29ba62072c2
Nathaly <launathalypastrana@gmail.com>
2024-09-14 17:00:52 -0500

    se agrega el enviroment para poder desplegarlo en railway

--- JetBoom/settings.py
+++ JetBoom/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
