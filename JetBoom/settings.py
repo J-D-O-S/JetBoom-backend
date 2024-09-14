aedd925ce52080e289f497e88e0ff1d42f4a5868
THE1st <jdos46juan@gmail.com>
2024-09-14 00:24:51 -0500

    Configuracion para deploy en base al siguiente video https://www.youtube.com/watch?v=erLOWlo6nGA

--- JetBoom/settings.py
+++ JetBoom/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = "JetBoom.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / env("SQLITE_DB_NAME", default="db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / env("SQLITE_DB_NAME", default="db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -130,7 +141,7 @@ AUTH_USER_MODEL = "Usuarios.Usuario"
 
 LANGUAGE_CODE = "es-co"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "UTC-5"
 
 USE_I18N = True
 
@@ -144,7 +155,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
