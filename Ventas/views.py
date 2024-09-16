5d42cb06dd3373b8a7576ddd8be74bde57693349
Nathaly <launathalypastrana@gmail.com>
2024-09-16 16:57:21 -0500

    se cambian de posicion a las carpetas

--- Ventas/views.py
+++ Ventas/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class SellView(TemplateView):
-    template_name = "pago.html"
+    template_name = "pago/pago.html"
     #  def get(self, request):
     #     return render(request, "pago.html")
