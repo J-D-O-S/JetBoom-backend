f9e34166f94270eea847529ca80ce26e160ed1cd
Nathaly <launathalypastrana@gmail.com>
2024-09-16 16:45:48 -0500

    se le da una url a la vista de actualiza datos

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -215,3 +215,6 @@ class CustomUploadImageView(LoginRequiredMixin, View):
         else:
             print(form.errors)
             return render(self.request, "perfil/perfil.html", {"form": form})
+        
+class actualizarDatosView(TemplateView):
+    template_name = "usuarios/actualizarDatos.html"            
