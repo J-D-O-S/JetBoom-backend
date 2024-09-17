94952de44fd6bd66eb726b23aa0c273980bb7895
Nathaly <launathalypastrana@gmail.com>
2024-09-16 20:51:21 -0500

    se agregan las url de las vistas pasarela de pago y comprobante de pago

--- Ventas/views.py
+++ Ventas/views.py
@@ -3,5 +3,14 @@ from django.views.generic.base import TemplateView
 
 class SellView(TemplateView):
     template_name = "pago/pago.html"
+
+class comprobanteView(TemplateView):
+    template_name = "pago/comprobantePago.html"  
+
+class pasarelaView(TemplateView):
+    template_name = "pasarela_pago/pasarelaPago.html"      
     #  def get(self, request):
     #     return render(request, "pago.html")
+
+
+    
