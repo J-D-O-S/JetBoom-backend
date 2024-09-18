5e57b2247ec7e107a3ab5ff338fed968c3db88eb
THE1st <jdos46juan@gmail.com>
2024-09-17 20:19:05 -0500

    cambios sobre la vista de pago en proceso, se detiene por que se va a hacer el merge con la rama de Nathaly

--- Ventas/views.py
+++ Ventas/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import date
 
 
 class SellView(LoginRequiredMixin, TemplateView):
     template_name = "pago.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["fecha_min"] = date(1924, 1, 1)
+        context["fecha_max"] = date(2006, 12, 31)
+        return context
 
     def get(self, request):
         return render(request, "pago.html")
